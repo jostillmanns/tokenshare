fe08b6dd038ed0b8af245feb15eb59e862868718
Joschka Tillmanns <joschka@tillmanns.me>
2016-08-08 11:22:27 +0200

    backend: extent tests

--- backend/server_test.go
+++ backend/server_test.go
@@ -3,20 +3,25 @@ package main
 import (
 	"bytes"
 	"encoding/hex"
+	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"sync"
 	"testing"
 	"tokenshare"
 )
 
 func TestCreate(t *testing.T) {
-	server, url, cookie, close := newTestServer(t)
+	server, testSrv, cookie, close := newTestServer(t)
 	defer close()
 
-	tok, err := tokenshare.Create(url+tokenshare.ReqCreate, cookie)
+	tok, err := tokenshare.Create(testSrv.URL+tokenshare.ReqCreate, cookie)
 	if err != nil {
 		t.Fatalf("create: %v", err)
 	}
@@ -37,7 +42,7 @@ func TestCreate(t *testing.T) {
 }
 
 func TestTransfer(t *testing.T) {
-	server, url, _, close := newTestServer(t)
+	server, testSrv, _, close := newTestServer(t)
 	defer close()
 
 	tok, err := server.generate()
@@ -48,9 +53,9 @@ func TestTransfer(t *testing.T) {
 	id := hex.EncodeToString(tok.ID)
 	name := "foobar"
 
-	buf := []byte("GREETING")
+	buf := make([]byte, 1024*1024*50)
 	if err := tokenshare.Transfer(
-		url+tokenshare.ReqTransfer,
+		testSrv.URL+tokenshare.ReqTransfer,
 		name,
 		id,
 		buf,
@@ -71,7 +76,7 @@ func TestTransfer(t *testing.T) {
 }
 
 func TestList(t *testing.T) {
-	server, url, cookie, close := newTestServer(t)
+	server, testSrv, cookie, close := newTestServer(t)
 	defer close()
 
 	tok, err := server.generate()
@@ -83,7 +88,7 @@ func TestList(t *testing.T) {
 		t.Fatalf("single: %v", err)
 	}
 
-	toks, err := tokenshare.List(url+tokenshare.ReqList, cookie)
+	toks, err := tokenshare.List(testSrv.URL+tokenshare.ReqList, cookie)
 	if err != nil {
 		t.Fatalf("list: %v", err)
 	}
@@ -98,7 +103,7 @@ func TestList(t *testing.T) {
 }
 
 func TestDownload(t *testing.T) {
-	server, url, _, close := newTestServer(t)
+	server, testSrv, _, close := newTestServer(t)
 	defer close()
 
 	tok, err := server.generate()
@@ -122,7 +127,7 @@ func TestDownload(t *testing.T) {
 		t.Fatalf("write file: %v", err)
 	}
 
-	res, err := tokenshare.Download(url+tokenshare.ReqDownload, hex.EncodeToString(tok.ID))
+	res, err := tokenshare.Download(testSrv.URL+tokenshare.ReqDownload, hex.EncodeToString(tok.ID))
 	if err != nil {
 		t.Fatalf("download: %v", err)
 	}
@@ -132,27 +137,136 @@ func TestDownload(t *testing.T) {
 	}
 }
 
-func newTestServer(t *testing.T) (*server, string, *http.Cookie, func()) {
+func TestTransferBrowser(t *testing.T) {
+	once := sync.Once{}
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+
+	s, srv, _, clear := newTestServer(t)
+	defer clear()
+
+	bufSize := int64(1024 * 1024 * 50)
+
+	s.mux.HandleFunc("/transfertest", func(w http.ResponseWriter, req *http.Request) {
+		once.Do(func() {
+			defer wg.Done()
+			if err := req.ParseMultipartForm(s.maxMemory); err != nil {
+				t.Fatalf("parse multipart: %v", err)
+			}
+
+			file, _, err := req.FormFile(tokenshare.File)
+			if err != nil {
+				t.Fatalf("file open: %v", err)
+			}
+
+			buffer := bytes.NewBuffer(nil)
+			n, err := io.Copy(buffer, file)
+			if err != nil {
+				t.Fatalf("copy: %v", err)
+			}
+
+			defer func() {
+				_ = file.Close()
+			}()
+
+			if n != bufSize {
+				t.Fatalf("%d != %d", 1024*1024*50, n)
+			}
+		})
+	})
+
+	srv.Close()
+	srv = httptest.NewServer(s.mux)
+
+	install := exec.Command("gopherjs", "install", "tokenshare/test")
+	if err := install.Run(); err != nil {
+		t.Fatalf("install: %v", err)
+	}
+
+	datadir, err := ioutil.TempDir("", "tokenshare-chrome")
+	if err != nil {
+		t.Fatalf("tmpdir: %v", err)
+	}
+	defer os.RemoveAll(datadir)
+
+	u, err := url.Parse(srv.URL)
+	if err != nil {
+		t.Fatalf("url parse: %v", err)
+	}
+
+	u.Path = "testing"
+	browser := exec.Command("chromium", "--incognito", fmt.Sprintf("--user-data-dir=%s", datadir), u.String())
+	if err := browser.Start(); err != nil {
+		t.Fatalf("chrome: %v", err)
+	}
+
+	wg.Wait()
+	if err := browser.Process.Kill(); err != nil {
+		t.Fatalf("process kill: %v", err)
+	}
+}
+
+func newTestServer(t *testing.T) (*server, *httptest.Server, *http.Cookie, func()) {
 	storage, err := ioutil.TempDir("", "tokenshare")
 	if err != nil {
 		t.Fatalf("tmpdir: %v", err)
 	}
 
+	www, err := ioutil.TempDir("", "tokenshare-www")
+	if err != nil {
+		t.Fatalf("tmpdir: %v", err)
+	}
+
+	// copy necessary files
+	for _, f := range []string{"index.html", "testing.html"} {
+		src, err := os.Open(filepath.Join("www", f))
+		if err != nil {
+			t.Fatalf("open: %v", err)
+		}
+
+		dest, err := os.OpenFile(filepath.Join(www, f), os.O_CREATE|os.O_WRONLY, 0600)
+		if err != nil {
+			t.Fatalf("open: %v", err)
+		}
+
+		if _, err := io.Copy(dest, src); err != nil {
+			t.Fatalf("copy: %v", err)
+		}
+
+		_ = src.Close()
+		_ = dest.Close()
+	}
+
 	bolt := filepath.Join(storage, "bolt")
 
-	s, err := newServer(
+	s, err := newSrv(
 		bolt,
 		"token",
 		storage,
-		"www",
+		www,
 		"user",
 		"pass",
 		16,
-		int64(20*20*1024),
+		int64(1024*1024*1024),
 	)
 	if err != nil {
 		t.Fatalf("server: %v", err)
 	}
+
+	s.mux.HandleFunc("/testing", func(w http.ResponseWriter, req *http.Request) {
+		s.file(w, www, "testing.html", "text/html; charset-utf-8")
+	})
+
+	h := func(call string) func(w http.ResponseWriter, req *http.Request) {
+		return func(w http.ResponseWriter, req *http.Request) {
+			path := os.Getenv("GOPATH")
+			path = filepath.Join(path, "bin")
+			s.file(w, path, call, "application/javascript")
+		}
+	}
+
+	s.mux.HandleFunc("/testing.js", h("test.js"))
+	s.mux.HandleFunc("/testing.js.map", h("test.js.map"))
 
 	server := httptest.NewServer(s.mux)
 
@@ -183,8 +297,9 @@ func newTestServer(t *testing.T) (*server, string, *http.Cookie, func()) {
 		t.Fatalf("missing cookie")
 	}
 
-	return s, server.URL, cookie, func() {
-		os.RemoveAll(storage)
+	return s, server, cookie, func() {
+		_ = os.RemoveAll(storage)
+		_ = os.RemoveAll(www)
 		server.Close()
 	}
 }
